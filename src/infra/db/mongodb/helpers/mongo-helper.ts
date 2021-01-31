a084672ba776f2e9681895195345a3a48a19fe04
Davi <davifeitosa.dev@protonmail.com>
2021-01-31 12:36:18 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient, // assim que fazemos para o typescript funcionar em objetos
@@ -10,6 +10,9 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 
 }
