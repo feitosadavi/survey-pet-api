3d990800fbc028ab42be2f6707f954f3a9d1dd0a
Davi <davifeitosa.dev@protonmail.com>
2021-01-31 18:38:06 -0300

    test: ensure request return xml Content Type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,11 +3,21 @@ import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   test('Should return default Content Type as json', async () => {
-    app.get('/test_content_type', (req, res) => {
-      res.send()
+    app.get('/test_content_type_json', (req, res) => {
+      res.send('')
     })
     await request(app)
-      .get('/test_content_type')
+      .get('/test_content_type_json')
       .expect('content-type', /json/) // se existir .json na resposta, ele aceita
+  })
+
+  test('Should return xml when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
