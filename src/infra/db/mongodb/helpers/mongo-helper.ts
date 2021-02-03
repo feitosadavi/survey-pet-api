b0eee227dc6597ca3610ddaeb843d0277964463a
Davi <davifeitosa.dev@protonmail.com>
2021-02-03 17:52:47 -0300

    refactor: change ok function name to serverSuccess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,13 +3,10 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient, // assim que fazemos para o typescript funcionar em objetos
   async connect (url: string): Promise<void> {
-    console.log(url)
-
     this.client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    console.log('connected')
   },
   async disconnect (): Promise<void> {
     await this.client.close()
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -11,7 +11,7 @@ export const serverError = (): HttpResponse => ({
   body: new ServerError()
 })
 
-export const ok = (data: any): HttpResponse => ({
+export const serverSuccess = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
 })
