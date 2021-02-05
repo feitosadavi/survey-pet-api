5c3748f7190a573e25808fe6ce253d60ebfb547b
Davi <davifeitosa.dev@protonmail.com>
2021-02-05 18:11:15 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,41 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeController = () => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse = {
+        body: {
+          email: 'davifeitosa.dev@protonmail.com',
+          name: 'Davi',
+          password: '123',
+          passwordConfirmation: '123'
+        },
+        statusCode: 200
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse = {
-          body: {
-            email: 'davifeitosa.dev@protonmail.com',
-            name: 'Davi',
-            password: '123',
-            passwordConfirmation: '123'
-          },
-          statusCode: 200
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub = new ControllerStub()
-    const sut = new LogControllerDecorator(controllerStub)
-    const handleSpy = jest.spyOn(sut, 'handle')
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
