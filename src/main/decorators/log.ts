b8d86a9cfbc88a91fe2e9c61df43eb861889d198
Davi <davifeitosa.dev@protonmail.com>
2021-02-05 18:01:26 -0300

    feat: ensure LogControllerDecorator calls controller with correct values

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+// com o decorator eu adiciono um comportamento ao controlador, sem modificar o controlador
+// A classe que vamos decorar, deve do mesmo tipo da classe que estamos implementando ou herdando
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest)
+    return null
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,9 +2,11 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
 
   const accountMongoRepository = new AccountMongoRepository()
@@ -14,5 +16,5 @@ export const makeSignUpController = (): SignUpController => {
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
   const signUpController = new SignUpController(emailValidator, dbAddAccount)
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
