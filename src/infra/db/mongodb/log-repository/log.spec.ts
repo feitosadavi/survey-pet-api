6010a3a46d3dd5463fa6677a86c2f15e894713f2
Davi <davifeitosa.dev@protonmail.com>
2021-02-06 19:04:38 -0300

    test: move sut creation to a factory method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -1,6 +1,11 @@
 import { Collection } from 'mongodb'
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LogMongoRepository } from './log'
+
+const makeSut = (): LogErrorRepository => {
+  return new LogMongoRepository()
+}
 
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
@@ -18,7 +23,7 @@ describe('Log Mongo Repository', () => {
     await errorCollection.deleteMany({})
   })
   test('should create an error log on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1) // eu inseri 1 erro, então espero que a quantidade de registros na collection count seja 1
