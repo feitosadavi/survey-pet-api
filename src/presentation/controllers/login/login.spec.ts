b2acbc50104dbd366ffd5661f49e52857962a30a
Davi <davifeitosa.dev@protonmail.com>
2021-02-22 17:47:57 -0300

    test: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
@@ -56,7 +56,6 @@ describe('Login Controller', () => {
 
   test('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
-
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
     const httResponse = await sut.handle(makeFakeRequest())
     expect(httResponse).toEqual(badRequest(new InvalidParamError('email')))
@@ -64,9 +63,18 @@ describe('Login Controller', () => {
 
   test('Should call EmailValidator with correct email', async () => {
     const { emailValidatorStub, sut } = makeSut()
-
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
     await sut.handle(makeFakeRequest())
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { emailValidatorStub, sut } = makeSut()
+
+    // substitui toda a implementação do método
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
