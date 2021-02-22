9d26381ed9e51becb1fd6118780fba8bf770bc30
Davi <davifeitosa.dev@protonmail.com>
2021-02-22 19:48:11 -0300

    test: ensure return 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, serverSuccess } from '../../helpers/http-helper'
 import { Authentication, EmailValidator, HttpRequest } from './login-protocols'
 import { LoginController } from './login'
 
@@ -126,5 +126,11 @@ describe('Login Controller', () => {
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 if valid credentials are provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverSuccess({ accessToken: 'any_token' }))
   })
 })
