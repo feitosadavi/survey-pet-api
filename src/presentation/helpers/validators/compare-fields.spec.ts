8812341f8ad005c4419d7830b17961ee187cdd84
Davi <davifeitosa.dev@protonmail.com>
2021-02-25 17:23:44 -0300

    test: ensure RequiredFieldValidate returns a MissingParamError if validations fails

---
+++ src/presentation/helpers/validators/compare-fields.spec.ts
@@ -0,0 +1,32 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields'
+import { Validation } from './validation'
+
+interface SutTypes {
+  sut: Validation
+}
+
+const makeSut = (fieldName: string, fieldNameToCompare: string): SutTypes => {
+  const sut = new CompareFieldsValidation(fieldName, fieldNameToCompare)
+  return {
+    sut
+  }
+}
+
+describe('RequireFieldsValidation', () => {
+  test('Should return InvalidParamError if CompareFieldsValidation detects missing param', () => {
+    const { sut } = makeSut('password', 'passwordConfirmation') // quero comparar estes dois campos
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password123'
+      }
+    }
+
+    const error = sut.validate(httpRequest.body)
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
