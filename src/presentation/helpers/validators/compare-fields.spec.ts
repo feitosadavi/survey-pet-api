06abaae3b6f350e61ff58136bbdd75169ab18ffb
Davi <davifeitosa.dev@protonmail.com>
2021-02-25 17:55:47 -0300

    test: CompareFieldsValidation returns nothing if validation succeed

--- src/presentation/helpers/validators/compare-fields.spec.ts
+++ src/presentation/helpers/validators/compare-fields.spec.ts
@@ -15,8 +15,14 @@ const makeSut = (fieldName: string, fieldNameToCompare: string): SutTypes => {
 
 describe('CompareFieldsValidation', () => {
   test('Should CompareFieldsValidation returns InvalidParamError if validation fails', () => {
-    const { sut } = makeSut('field', 'fieldToCompare') // quero comparar estes dois campos
+    const { sut } = makeSut('field', 'fieldToCompare')
     const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should CompareFieldsValidation returns nothing if validation succeed', () => {
+    const { sut } = makeSut('field', 'fieldToCompare')
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
