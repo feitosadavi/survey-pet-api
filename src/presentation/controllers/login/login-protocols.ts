87bd798b604486a9ab2c7112272755e04dbc1233
Davi <davifeitosa.dev@protonmail.com>
2021-02-26 17:45:43 -0300

    refactor: correct protocol name

--- src/presentation/controllers/login/login-protocols.ts
+++ src/presentation/controllers/login/login-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../../domain/usecases/authetication'
+export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/email-validator'
 export * from '../../protocols/http'
 export * from '../../protocols/controller'
