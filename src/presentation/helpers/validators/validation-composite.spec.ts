2c67486254c9b18392202bf02af4ccdfd8dc97ce
Davi <davifeitosa.dev@protonmail.com>
2021-02-26 17:06:13 -0300

    test: ensure ValidationComposite returns nothing if all validations succeed

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,6 +1,11 @@
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
 const makeValidationStub = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
@@ -10,12 +15,30 @@ const makeValidationStub = (): Validation => {
   return new ValidationStub()
 }
 
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const validationStub = makeValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut } = makeSut()
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new Error())
+  })
+
+  test('Should return nothing if all validations succeed', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockImplementationOnce((input: any): undefined => {
+      return undefined
+    })
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
   })
 })
