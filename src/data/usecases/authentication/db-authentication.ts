0071ebf0a02bf8e906f69750fa9ea4ae1ff4d68b
Davi <davifeitosa.dev@protonmail.com>
2021-02-27 14:38:35 -0300

    feat: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -9,7 +9,7 @@ export class DbAuthentication implements Authentication {
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
-    return null
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (!account) return null
   }
 }
