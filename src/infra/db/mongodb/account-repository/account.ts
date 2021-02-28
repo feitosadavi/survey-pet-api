9fed10348c67047759631156b7155dca91512433
Davi <davifeitosa.dev@protonmail.com>
2021-02-28 00:16:28 -0300

    feat: ensure AccountRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,20 +1,30 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/usecases/authentication/db-authentication-protocols'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = await MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(accountData)
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountsCollection.insertOne(accountData)
     const account = result.ops[0]
     return MongoHelper.map(account) // converte o _id para id
   }
 
   async loadByEmail (email: string): Promise<AccountModel> {
-    const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ email })
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountsCollection.findOne({ email })
     return account && MongoHelper.map(account)
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.updateOne({ _id: id },
+      {
+        $set: { accessToken: token }
+      }
+    )
   }
 }
