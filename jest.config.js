e405b09258d49faef1d5e88bd6c91858236e77a8
Davi <davifeitosa.dev@protonmail.com>
2021-03-02 13:23:59 -0300

    chore: exclude protocol files from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/protocols/*',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
