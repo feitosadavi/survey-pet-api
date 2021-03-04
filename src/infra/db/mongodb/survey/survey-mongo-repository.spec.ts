a6a74d3f41034f48d23b2a38a0e7f950235fc476
Davi <davifeitosa.dev@protonmail.com>
2021-03-04 19:38:54 -0300

    test: ensure SurveyMongoRepository creates a survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+describe('SurveyMongo Repository', () => {
+  let surveyCollection: Collection
+
+  // antes e depois de cada teste de integração, precisamos conectar e desconectar do banco
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  // removo todos os registros da tabela antes de cada teste. Para não populuir as tabelas
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should create a survey on add success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        { answer: 'other_answer' }
+      ]
+    })
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
