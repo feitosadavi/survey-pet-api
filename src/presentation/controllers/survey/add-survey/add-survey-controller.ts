9c3622b127dbc2cbf10ae8958ddaa12744f63614
Davi <davifeitosa.dev@protonmail.com>
2021-03-04 15:42:31 -0300

    feat: ensure AddSurvey Controller calls validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,13 @@
+import { HttpResponse } from '../../../protocols'
+import { Controller, HttpRequest, Validation } from './add-survey-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
+    return new Promise(resolve => resolve(null))
+  }
+}
