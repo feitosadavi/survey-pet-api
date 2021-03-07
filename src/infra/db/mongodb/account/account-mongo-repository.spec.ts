ba428f36233b4796f7a9e696dc756e2c125c8eb4
Davi <davifeitosa.dev@protonmail.com>
2021-03-07 20:19:30 -0300

    test: ensure AccountMongoRepository returns null on loadByToken with invalid role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -85,7 +85,7 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    test('Should return an account on loadByToken success with role', async () => {
+    test('Should return an account on loadByToken success with admin role', async () => {
       const sut = makeSut()
 
       await accountCollection.insertOne({
@@ -93,10 +93,10 @@ describe('Account Mongo Repository', () => {
         email: 'any_email@email.com',
         password: 'any_password',
         accessToken: 'any_token',
-        role: 'any_role'
+        role: 'admin'
       })
 
-      const account = await sut.loadByToken('any_token', 'any_role')
+      const account = await sut.loadByToken('any_token', 'admin')
       expect(account).toBeTruthy()
       expect(account.id).toBeTruthy()
       expect(account.name).toBe('any_name')
@@ -104,9 +104,23 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
+    test('Should return null on loadByToken with invalid role', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+
+      const account = await sut.loadByToken('any_token', 'admin')
+      expect(account).toBeFalsy()
+    })
+
     test('Should return null if loadByToken fails', async () => {
       const sut = makeSut()
-      const account = await sut.loadByToken('any_token', 'any_role')
+      const account = await sut.loadByToken('any_token', 'admin')
       expect(account).toBeFalsy()
     })
   })
