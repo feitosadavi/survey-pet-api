1dd17732ce0cea9d464ff3a362ce10b1a55fc9be
Davi <davifeitosa.dev@protonmail.com>
2021-03-07 19:02:21 -0300

    feat: ensure add survey returns 204 on add survey success

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -3,12 +3,12 @@ import { HttpRequest, Middleware } from '../../presentation/protocols'
 
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const httpRequest: HttpRequest = {
+    const httpRequest: HttpRequest = { // adapto o request
       headers: req.headers
     }
     const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
-      Object.assign(req, httpResponse.body)
+      Object.assign(req, httpResponse.body) // coloco a resposta na requisição
       next()
     } else {
       res.status(httpResponse.statusCode).json({
