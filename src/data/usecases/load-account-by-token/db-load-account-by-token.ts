c6e3404e4b1867355d93fc273a577c2b543f1339
Davi <davifeitosa.dev@protonmail.com>
2021-03-07 19:01:06 -0300

    refactor: change const name token to isValidToken

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -10,9 +10,10 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   ) {}
 
   async load (accessToken: string, role?: string): Promise<AccountModel> {
-    const token = await this.decrypter.decrypt(accessToken)
-    if (token) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+    const isValidToken = await this.decrypter.decrypt(accessToken)
+
+    if (isValidToken) {
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
         return account
       }
