2b1bff6ed844c17cdd9e3cce501bf3c568b67704
Davi <davifeitosa.dev@protonmail.com>
2021-03-07 18:25:42 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -19,7 +19,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('Should return 204 on add survey success ', async () => {
+    test('Should return 403 on add survey without accessToken ', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -39,7 +39,7 @@ describe('Survey Routes', () => {
             }
           ]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
