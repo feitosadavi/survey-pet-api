ca8d87cc1243f8c60af858f4d83811780cf0461c
Davi <davifeitosa.dev@protonmail.com>
2021-03-07 21:38:34 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken success if user is admin

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -118,6 +118,25 @@ describe('Account Mongo Repository', () => {
       expect(account).toBeFalsy()
     })
 
+    test('Should return an account on loadByToken success if user is admin', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin'
+      })
+
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@email.com')
+      expect(account.password).toBe('any_password')
+    })
+
     test('Should return null if loadByToken fails', async () => {
       const sut = makeSut()
       const account = await sut.loadByToken('any_token', 'admin')
