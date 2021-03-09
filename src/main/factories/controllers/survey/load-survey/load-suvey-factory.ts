71b3904502ba0f4d7643879e7b81e2801554ccd9
Davi <davifeitosa.dev@protonmail.com>
2021-03-09 14:08:04 -0300

    feat: ensure load surveys route returns 204 if surveys collection is empty

---
+++ src/main/factories/controllers/survey/load-survey/load-suvey-factory.ts
@@ -0,0 +1,10 @@
+import { DbLoadSurveys } from '../../../../../data/usecases/load-survey/db-load-survey'
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-survey/load-surveys'
+import { Controller } from '../../../../../presentation/protocols'
+
+export const makeLoadSurveyController = (): Controller => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  const dbLoadSurveys = new DbLoadSurveys(surveyMongoRepository)
+  return new LoadSurveysController(dbLoadSurveys)
+}
