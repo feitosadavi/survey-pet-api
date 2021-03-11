cacb00b7f68028cdac36da0d50803dbb88fe4ed1
Davi <davifeitosa.dev@protonmail.com>
2021-03-11 18:17:24 -0300

    feat: ensure DbLoadSurveyById calls LoadSurvetByIdRepository with correct values

--- src/domain/usecases/save-survey.ts
+++ src/domain/usecases/save-survey.ts
@@ -3,5 +3,5 @@ import { SurveyResultModel } from '../models/survey-result'
 type AddSurveyResultModel = Omit<SurveyResultModel, 'id'>
 
 export interface SaveSurveyResult {
-  save (data: SurveyResultModel): Promise<SurveyResultModel>
+  save (data: AddSurveyResultModel): Promise<SurveyResultModel>
 }
