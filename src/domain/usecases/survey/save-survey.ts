1e3d7236e5a04e940a189bd90310489964c3a037
Davi <davifeitosa.dev@protonmail.com>
2021-03-11 23:02:50 -0300

    fix: export AddSurveyResultModel and fix save method return

--- src/domain/usecases/survey/save-survey.ts
+++ src/domain/usecases/survey/save-survey.ts
@@ -1,7 +1,7 @@
 import { SurveyResultModel } from '../../models/survey-result'
 
-type AddSurveyResultModel = Omit<SurveyResultModel, 'id'>
+export type AddSurveyResultModel = Omit<SurveyResultModel, 'id'>
 
 export interface SaveSurveyResult {
-  save (data: AddSurveyResultModel): Promise<SurveyResultModel>
+  save (data: AddSurveyResultModel): Promise<void>
 }
