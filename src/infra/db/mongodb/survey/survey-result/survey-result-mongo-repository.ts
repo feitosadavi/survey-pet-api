2ab425b8697cbdc2a31e6f0f1dc7a33810bfc2fb
Davi <davifeitosa.dev@protonmail.com>
2021-03-12 14:57:34 -0300

    feat: ensure SurveyResultMongoRepository adds a survey result if its new

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,24 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { AddSurveyResultModel } from '@/domain/usecases/survey/save-survey'
+import { MongoHelper } from '../../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async saveResult (data: AddSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultsCollection = MongoHelper.getCollection('surveyResults')
+    const res = await (await surveyResultsCollection).findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+
+    return MongoHelper.map(res.value)
+  }
+}
