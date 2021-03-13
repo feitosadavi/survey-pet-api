4a7a133f57b1658d149e084be3908490d25c0b3f
Davi <davifeitosa.dev@protonmail.com>
2021-03-13 15:44:06 -0300

    feat: ensure SaveSurveyController returns surveys on load success

--- src/presentation/controllers/survey-result/save-survey-result.ts
+++ src/presentation/controllers/survey-result/save-survey-result.ts
@@ -1,10 +1,11 @@
+import { serverSuccess } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './save-survey-result-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveys = await this.loadSurveys.load()
+    return serverSuccess(surveys)
   }
 }
