6ebddeb1416691bfbc6b8be7eef1b984ed370c7c
Davi <davifeitosa.dev@protonmail.com>
2021-03-16 17:19:19 -0300

    feat: add swager api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Survey Pet API',
+    description: 'Uma API para a criação de enquetes relacionadas à pets',
+    version: '1.0.0'
+  }
+}
