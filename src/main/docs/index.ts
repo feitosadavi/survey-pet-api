52b9ebd987aeb8c817093369f4f558ce264e02b9
Davi <davifeitosa.dev@protonmail.com>
2021-03-16 21:50:17 -0300

    docs: add login docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { LoginParamsSchema } from './schemas/login-params-schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Survey Pet API',
     description: 'Uma API para a criação de enquetes relacionadas à pets',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Account'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: LoginParamsSchema
   }
 }
